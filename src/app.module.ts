cab10ea4d9b7e9c80164924b393d73f3519c7a0a
mobintahmasbi <mobintahmasbi13801202@gmail.com>
2024-12-19 12:19:24 +0330

    create development enviroment variables and add enviroment variables to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/SareVaght')],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.development.env'
+  }),
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      uri: configService.get<string>('DATABASE_URL')
+    }),
+    inject: [ConfigService]
+  })],
   controllers: [],
   providers: [],
 })
