7d493ae333149ee877885f522d814516b57e4835
mobintahmasbi <mobintahmasbi13801202@gmail.com>
2024-12-20 21:00:43 +0330

    add business model to business module

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { BusinessController } from "./business.controller";
 import { BusinessService } from "./business.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Business, BusinessSchema } from "./schema/business.schema";
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: Business.name, schema: BusinessSchema }])],
     controllers: [BusinessController],
     providers: [BusinessService]
 })
